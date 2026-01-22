8a797e21c3664f263a3a62ae9b547f62e79a5641
Madhu <madhuthakur2321998@gmail.com>
2026-01-22 16:53:32 +0530

    Adding meals summary

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
+        <h1>MealsPoint</h1>
         <CartButton />
       </header>
 
